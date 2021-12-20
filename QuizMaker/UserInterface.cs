6fdf7509649dd6fb5913d4ad87d125c6d3f09a86
mirmic80 <mirco.michel@bluewin.ch>
2021-12-20 10:50:40 +0100

    Add project files.

---
+++ QuizMaker/UserInterface.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizMaker
+{
+    public static class UserInterface
+    {
+        public static void DisplayWelcomeScreen()
+        {
+            Console.WriteLine("************************************************************");
+            Console.WriteLine("Press any key to start,(ESC) to exit/playout. Enjoy the Quis");
+            Console.WriteLine("************************************************************");
+        }
+    }
+}
